74933976d92ff7258782d6125425ef6dc3365a5e
mbikal <mbikal85@gmail.com>
2026-01-26 19:51:30 +0545

    packaging content

--- plag-system/__init__.py
+++ plag-system/__init__.py
@@ -1,6 +1,9 @@
 """Plagiarism system helpers."""
 # pylint: disable=invalid-name
 
-from .checker import analyze_and_sign, analyze_file, annotate_pdf, ensure_keypair
+try:
+    from .checker import analyze_and_sign, analyze_file, annotate_pdf, ensure_keypair
+except ImportError:  # pragma: no cover - fallback for direct execution
+    from checker import analyze_and_sign, analyze_file, annotate_pdf, ensure_keypair
 
 __all__ = ["analyze_and_sign", "analyze_file", "annotate_pdf", "ensure_keypair"]
